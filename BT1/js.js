3092c449fc532cde76d27ddef34980214538c04c
DevFEnddd <88378029+DevFEnddd@users.noreply.github.com>
2021-08-09 17:33:36 +0700

    Update js.js

--- BT1/js.js
+++ BT1/js.js
@@ -1,7 +1,7 @@
 //tính tổng các số từ 1 > 10
 let a = 10;
-let tong = 0;
+let total = 0;
 for (let i = 1; i <= 10; i++) {
-    tong += i;
+    total += i;
 }
-alert(`Tổng các số từ 1 đến 10 là: ${tong}`);
+alert(`Tổng các số từ 1 đến 10 là: ${total}`);
