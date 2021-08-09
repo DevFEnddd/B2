3a88c4e31c218b927d8f0afa824fa1b388273201
DevFEnddd <88378029+DevFEnddd@users.noreply.github.com>
2021-08-09 17:35:11 +0700

    Update js.js

--- BT2/js.js
+++ BT2/js.js
@@ -1,15 +1,15 @@
 let number;
-let giaiThua;
+let factorial;
 while (true) {
     number = prompt('Enter number');
-    giaiThua = 1;
+    factorial = 1;
     if (number < 0) {
         alert('Không tồn tại giai thừa của số âm')
     } else {
         for (let i = 1; i <= number; i++) {
-            giaiThua *= i;
+            factorial *= i;
         }
-        alert(`${number}! = ${giaiThua}`)
+        alert(`${number}! = ${factorial}`)
         break;
     }
 
